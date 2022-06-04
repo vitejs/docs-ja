02eed9a915ab9bf079c748191d33ea48cf912ebc
hiro-lapis <53115998+hiro-lapis@users.noreply.github.com>
2022-06-04 20:50:47 +0900

    docs: vitepress next updates  (#477)
    * docs: use vitepress next
    
    * docs: use vitepress next
    
    * docs: vitepress next updates
    
    * modify localeLinks text
    
    * remove typo
    
    * add space around alphabet

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,16 +3,28 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'Vite',
   description: '次世代フロントエンドツール',
+
   head: [['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }]],
+
   vue: {
     reactivityTransform: true
   },
+
   themeConfig: {
-    repo: 'vitejs/docs-ja',
     logo: '/logo.svg',
-    docsBranch: 'main',
-    editLinks: true,
-    editLinkText: 'このページへの変更を提案する',
+
+    editLink: {
+      repo: 'vitejs/docs-ja',
+      branch: 'main',
+      dir: 'docs',
+      text: 'このページへの変更を提案する'
+    },
+
+    socialLinks: [
+      { icon: 'twitter', link: 'https://twitter.com/vite_js' },
+      { icon: 'discord', link: 'https://chat.vitejs.dev' },
+      { icon: 'github', link: 'https://github.com/vitejs/vite' }
+    ],
 
     algolia: {
       apiKey: 'b573aa848fd57fb47d693b531297403c',
@@ -25,6 +37,19 @@ export default defineConfig({
     carbonAds: {
       carbon: 'CEBIEK3N',
       placement: 'vitejsdev'
+    },
+
+    localeLinks: {
+      text: 'English',
+      items: [
+        { text: '简体中文', link: 'https://cn.vitejs.dev' },
+        { text: '日本語', link: 'https://ja.vitejs.dev' }
+      ]
+    },
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2019-present Evan You & Vite Contributors'
     },
 
     nav: [
@@ -69,33 +94,13 @@ export default defineConfig({
           }
         ]
       },
-      {
-        text: '言語',
-        items: [
-          {
-            text: 'English',
-            link: 'https://vitejs.dev'
-          },
-          {
-            text: '简体中文',
-            link: 'https://cn.vitejs.dev'
-          },
-          {
-            text: '日本語',
-            link: 'https://ja.vitejs.dev'
-          }
-        ]
-      }
     ],
 
     sidebar: {
-      '/config/': 'auto',
-      '/plugins': 'auto',
-      // catch-all fallback
-      '/': [
+      '/guide/': [
         {
           text: 'ガイド',
-          children: [
+          items: [
             {
               text: 'なぜ Vite なのか',
               link: '/guide/why'
@@ -152,7 +157,7 @@ export default defineConfig({
         },
         {
           text: 'API',
-          children: [
+          items: [
             {
               text: 'プラグイン API',
               link: '/guide/api-plugin'
