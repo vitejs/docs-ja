847c37277c012e3345de8d8f13bbfde5f8f84883
hiro-lapis <coolish555ddddd@gmail.com>
2022-06-05 09:36:06 +0900

    docs: update vitepress and add active match to nav

--- .vitepress/theme/components/HomeSponsors.vue
+++ .vitepress/theme/components/HomeSponsors.vue
@@ -8,6 +8,7 @@ const { data } = useSponsor()
   <VPHomeSponsors
     v-if="data"
     message="Vite is free and open source, made possible by wonderful sponsors."
+    action-text="Become a sponsor"
     action-link="https://github.com/sponsors/yyx990803"
     :data="data"
   />
