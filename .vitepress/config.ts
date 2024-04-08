0d7c3f2a54c845d2f00338742464afe8dd873f85
Mehdi <9340937+meduzen@users.noreply.github.com>
2024-04-08 16:07:01 +0200

    docs: add Mastodon to “resources” in docs navigation (#16370)

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -156,6 +156,10 @@ export default defineConfig({
           {
             items: [
               {
+                text: 'Mastodon',
+                link: 'https://elk.zone/m.webtoo.ls/@vite',
+              },
+              {
                 text: 'Twitter',
                 link: 'https://twitter.com/vite_js',
               },
