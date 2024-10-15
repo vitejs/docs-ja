746a59f418da94d8a67addbdcaf9ac9ec7986ea3
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2024-10-15 21:34:05 +0900

    docs: refactor environment api docs

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -322,12 +322,33 @@ export default defineConfig({
               link: '/guide/api-javascript',
             },
             {
-              text: 'Environment API',
+              text: '設定リファレンス',
+              link: '/config/',
+            },
+          ],
+        },
+        {
+          text: 'Environment API',
+          items: [
+            {
+              text: 'Introduction',
               link: '/guide/api-environment',
             },
             {
-              text: '設定リファレンス',
-              link: '/config/',
+              text: 'Environment instances',
+              link: '/guide/api-environment-instances',
+            },
+            {
+              text: 'Plugins',
+              link: '/guide/api-environment-plugins',
+            },
+            {
+              text: 'Frameworks',
+              link: '/guide/api-environment-frameworks',
+            },
+            {
+              text: 'Runtimes',
+              link: '/guide/api-environment-runtimes',
             },
           ],
         },
