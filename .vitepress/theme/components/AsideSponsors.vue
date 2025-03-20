cb52b9408ecdcaa3caca8ea8f668da1e502fb261
TAKAHASHI Shuuji <shuuji3@gmail.com>
2025-03-20 23:42:34 +0900

    docs: update to viteconf 2025 (#1882)
    * docs: update to viteconf 2025
    
    resolve #1881
    
    https://github.com/vitejs/vite/commit/cc2d623dca58ca3e006ecb1ffda89c16373bd20c の反映です。
    
    * Update .vitepress/theme/components/landing/1. hero-section/HeroSection.vue
    
    Co-authored-by: Jun Shindo <46585162+jay-es@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jun Shindo <46585162+jay-es@users.noreply.github.com>

--- .vitepress/theme/components/AsideSponsors.vue
+++ .vitepress/theme/components/AsideSponsors.vue
@@ -21,14 +21,14 @@ const sponsors = computed(() => {
 <template>
   <a
       class="viteconf"
-      href="https://viteconf.org/24/replay?utm=vite-sidebar"
+      href="https://viteconf.org/?utm=vite-sidebar"
       target="_blank"
   >
     <img width="22" height="22" src="/viteconf.svg" alt="ViteConf Logo" />
     <span>
       <p class="extra-info">Building Together</p>
-      <p class="heading">ViteConf 2024</p>
-      <p class="extra-info">リプレイを視聴する！</p>
+      <p class="heading">ViteConf 2025</p>
+      <p class="extra-info">初めて対面で開催！</p>
     </span>
   </a>
   <VPDocAsideSponsors v-if="data" :data="sponsors" />
